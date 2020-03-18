79afb145be6232e75fbb2959999d34d1beba5cfa
Kévin <kevin.ledevehat2018@campus-eni.fr>
2020-03-18 16:32:48 +0100

    initial commit

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,59 @@
+﻿using Module5_TP1.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Module5_TP1.Controllers
+{
+    public class ChatController : Controller
+    {
+        private List<Chat> meuteChats = Chat.GetMeuteDeChats();
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(meuteChats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            Chat chat = meuteChats.SingleOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(chat);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Chat chat = meuteChats.SingleOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                Chat chat = meuteChats.SingleOrDefault(c => c.Id == id);
+                if (chat != null)
+                {
+                    meuteChats.Remove(chat);
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Chat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Module5_TP1.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Age { get; set; }
+        public string Couleur { get; set; }
+
+        private static List<Chat> meuteChats;
+
+        private static void InitMeuteDeChats()
+        {
+            var i = 1;
+            meuteChats = new List<Chat>
+            {
+                new Chat{Id=i++,Nom = "Felix",Age = 3,Couleur = "Roux"},
+                new Chat{Id=i++,Nom = "Minette",Age = 1,Couleur = "Noire"},
+                new Chat{Id=i++,Nom = "Miss",Age = 10,Couleur = "Blanche"},
+                new Chat{Id=i++,Nom = "Garfield",Age = 6,Couleur = "Gris"},
+                new Chat{Id=i++,Nom = "Chatran",Age = 4,Couleur = "Fauve"},
+                new Chat{Id=i++,Nom = "Minou",Age = 2,Couleur = "Blanc"},
+                new Chat{Id=i,Nom = "Bichette",Age = 12,Couleur = "Rousse"}
+            };
+        }
+
+        public static List<Chat> GetMeuteDeChats()
+        {
+           if (meuteChats is null)
+            {
+                InitMeuteDeChats();
+            }
+            return meuteChats;
+        }
+    }
+}
